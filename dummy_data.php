00810c14be3fcabacc9431fee7fd60d1a5edc04f
Shipu Ahamed <shipuahamed01@gmail.com>
2019-09-10 22:46:59 +0600

    first commit

---
+++ dummy_data.php
@@ -0,0 +1,20 @@
+<?php
+$payerReference = '01770618575';
+$orders = [
+    [
+        'code'   => 'Order001',
+        'ref_no'   => 'R001',
+        'amount' => rand(1, 12)
+    ],
+    [
+        'code'   => 'Order002',
+        'ref_no'   => 'R002',
+        'amount' => rand(1, 12)
+    ],
+    [
+        'code'   => 'Order003',
+        'ref_no'   => 'R003',
+        'amount' => rand(1, 12)
+    ],
+];
+
